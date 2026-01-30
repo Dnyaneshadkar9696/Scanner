4946e592c94ef656a2b70445f25537168265a2d3
ADKAR DNYANESH RAMDAS <dnyaneshadkar@gmail.com>
2026-01-30 21:55:47 +0530

    Update MainScanner.java

--- MainScanner.java
+++ MainScanner.java
@@ -3,32 +3,39 @@ package scanner;
 public class MainScanner {
 	
 	public static void main(String args[]) {
-//		PrimeCheck pi = new PrimeCheck();
-//		pi.primeCheck();
-//		
-//		System.out.println();
-//		Factorial ff = new Factorial();
-//		ff.checkFactorial();
-//		
-//		System.out.println();
-//		Addition ad = new Addition();
-//		ad.addNumber();
-//		
-//		System.out.println();
-//		UserInfo us = new UserInfo();
-//		us.infoUser();
+		PrimeCheck pi = new PrimeCheck();
+		pi.primeCheck();
 		
-//		LargestNumber lar = new LargestNumber();
-//		lar.checkLargeNum();
+		System.out.println();
+		Factorial ff = new Factorial();
+		ff.checkFactorial();
 		
-//		AreaOfCircle area = new AreaOfCircle();
-//		area.checkCirle();
+		System.out.println();
+		Addition ad = new Addition();
+		ad.addNumber();
+		
+		System.out.println();
+		UserInfo us = new UserInfo();
+		us.infoUser();
+		
+		LargestNumber lar = new LargestNumber();
+		lar.checkLargeNum();
+		
+		AreaOfCircle area = new AreaOfCircle();
+		area.checkCirle();
 		
 		Sentence sent = new Sentence();
 		sent.printSentence();
+
+		MarkOfSubjects mar = new MarkOfSubjects();
+		mar.marksInp();
+		
+		Product pr = new Product();
+		pr.priceCheck();
 		
 		
 		
 	}
 
 }
+
